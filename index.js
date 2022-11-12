4e15d658bb4cbf2049e8c6bdea63031dc26fdf75
Peak Nattapat <peak@finnomena.com>
2022-11-12 19:13:38 +0700

    deploy app on gcp

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const app = require("./app");
 const { connectDB } = require("./configs/connect");
 
-app.listen(process.env.PORT, async () => {
+const PORT = process.env.PORT || 3405;
+app.listen(PORT, async () => {
   await connectDB();
-  console.log("PORT:", process.env.PORT.yellow);
+  console.log("PORT:", PORT.yellow);
 });
